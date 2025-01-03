042f0a3693220abde3b83bac5cd6ccb1a280c51d
nquidox <nquidox@gmail.com>
2025-01-03 22:15:17 +0300

    refaсtor: router link moved from view to card

--- src/components/merch/Card.vue
+++ src/components/merch/Card.vue
@@ -1,4 +1,7 @@
 <script>
+import {mapGetters, mapState} from "vuex";
+import axios from "axios";
+
 export default {
   name: "MerchCard",
   props: {
@@ -23,30 +26,84 @@ export default {
       required: true,
     },
   },
+
+  data() {
+    return {
+      image: null,
+    };
+  },
+
+  computed: {
+    ...mapGetters("authAndToken", ["token"]),
+  },
+
+  mounted() {
+    this.fetchImage();
+  },
+  methods: {
+    async fetchImage() {
+      try {
+        const response = await axios.get(`/images/${this.uuid}`, {
+          headers: this.getHeaders(this.rootState),
+          responseType: 'blob',
+        });
+
+        if (response.status === 200) {
+          this.image = URL.createObjectURL(response.data);
+        } else if (response.status === 204) {
+          this.image = null;
+        }
+
+      } catch (error) {
+        console.error('Error fetching image:', error);
+      }
+    },
+
+    getHeaders() {
+      return {
+        Authorization: `Bearer ${this.token}`,
+      };
+    },
+  },
 }
 </script>
 
 <template>
-<div class="col">
-  <div class="card shadow-sm">
-    <img src="https://placehold.co/400x225" class="card-img-left" alt="Placeholder">
-    <div class="card-body">
-      <p class="card-text">Name: <strong>{{ name }}</strong></p>
-      <p class="card-text">Link: <strong>{{ link }}</strong></p>
-      <p class="card-text">
-        <span class="me-3">Price: <strong>{{ new_price }}</strong></span>
-        <span class="me-3">Old price: <strong>{{ old_price }}</strong></span>
-      </p>
-      <div class="d-flex justify-content-between align-items-center">
-        <div class="btn-group">
-          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+  <router-link :to="{ name: 'merchdetail', params: { id: uuid } }" class="custom-no-underline">
+    <div class="col">
+      <div class="card shadow-sm">
+        <img v-if="image"
+             :src="image"
+             class="card-img-left"
+             width="400"
+             height="225"
+             alt="Placeholder">
+
+        <img v-else
+             src="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='400' height='225' viewBox='0 0 400 225'><rect width='100%' height='100%' fill='%23e0e0e0'/><text x='50%' y='50%' dominant-baseline='middle' text-anchor='middle' font-size='20' fill='%23000000'>Image placeholder</text></svg>"
+             class="card-img-left"
+             width="400"
+             height="225"
+             alt="Placeholder">
+
+        <div class="card-body">
+          <p class="card-text">Name: <strong>{{ name }}</strong></p>
+          <p class="card-text">Link: <strong>{{ link }}</strong></p>
+          <p class="card-text">
+            <span class="me-3">Price: <strong>{{ new_price }}</strong></span>
+            <span class="me-3">Old price: <strong>{{ old_price }}</strong></span>
+          </p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+              <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+            </div>
+            <small class="text-body-secondary">9 mins</small>
+          </div>
         </div>
-        <small class="text-body-secondary">9 mins</small>
       </div>
     </div>
-  </div>
-</div>
+  </router-link>
 </template>
 
 <style scoped>
--- src/views/merch/CollectionView.vue
+++ src/views/merch/CollectionView.vue
@@ -35,15 +35,13 @@ export default {
   <h1 class="text-center">Your collection, master {{ userName }}</h1>
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
       <div v-for="item in readAllMerch" :key="item">
-        <router-link :to="{ name: 'merchdetail', params: { id: item.MerchUuid } }" class="custom-no-underline">
-          <MerchCard
-              :name="item.name"
-              :link="item.link"
-              :uuid="item.MerchUuid"
-              :new_price="item.new_price"
-              :old_price="item.old_price"
-          />
-        </router-link>
+        <MerchCard
+            :name="item.name"
+            :link="item.link"
+            :uuid="item.MerchUuid"
+            :new_price="item.new_price"
+            :old_price="item.old_price"
+        />
       </div>
     </div>
   </div>
