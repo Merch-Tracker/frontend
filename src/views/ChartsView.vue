4d3fa014b05256c87cd4aa708cb5a30da31fbe46
nquidox <nquidox@gmail.com>
2025-01-31 15:33:45 +0300

    temporary: last and next updates time block added

--- src/views/ChartsView.vue
+++ src/views/ChartsView.vue
@@ -20,6 +20,8 @@ export default {
   data() {
     return {
       chartsData: null,
+      lastUpdate: null,
+      nextUpdate: null,
     }
   },
 
@@ -41,7 +43,34 @@ export default {
       catch (error) {
         console.log(error)
       }
+      this.lastUpdated() // temporary
     },
+
+    lastUpdated(){
+      if (this.chartsData != null) {
+        const lastElem = this.chartsData[0].prices
+        const last = new Date(this.chartsData[0].prices[lastElem.length - 1].created_at);
+        this.lastUpdate = this.formatTime(last);
+
+        const next = new Date(last);
+        next.setHours(next.getHours() + 6);
+        this.nextUpdate = this.formatTime(next);
+      }
+    },
+
+    formatTime(date){
+      const hours = String(date.getHours()).padStart(2, '0');
+      const minutes = String(date.getMinutes()).padStart(2, '0');
+      const seconds = String(date.getSeconds()).padStart(2, '0');
+
+      const day = date.getDate();
+      const month = date.toLocaleString('en-US', { month: 'long' });
+      const year = date.getFullYear();
+
+      return `${hours}:${minutes}:${seconds} ${day} ${month} ${year}`;
+    },
+
+
   },
 
   mounted() {
@@ -52,6 +81,13 @@ export default {
 
 <template>
   <div class="container">
+    <div class="card mt-2 mb-2">
+      <div class="row p-2">
+        <div class="col-6 d-flex text-center justify-content-center align-items-center">Last updated: {{ lastUpdate }} UTC</div>
+        <div class="col-6 d-flex text-center justify-content-center align-items-center">Next update: {{ nextUpdate }} UTC</div>
+      </div>
+    </div>
+
     <PeriodSelector class="mt-2" @update:count="selectorUpdate" />
     <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 justify-content-evenly align-items-stretch mt-2">
       <div v-for="item in chartsData" :key="item" class="mt-2">
